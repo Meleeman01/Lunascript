a938c556b97c1eaa9d9e931afeab3ba54300f262
meleeman01 <meleeman01@live.com>
2020-12-07 15:46:43 -0600

    added singleline comments, and definitions and tokens lists.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lunascript
 a lua like language that compiles to javascript.
+
+so far it can do single line lua comments and output to console, the goal will eventually be to compile this down into javascript
--- compiler.js
+++ compiler.js
@@ -2,4 +2,42 @@
 const fs = require('fs');
 const file = fs.readFileSync("example.luna","utf8");
 //lexer init
-console.log(file.toString());
+const lexer = file.toString().split('\n') //produce an AST (abstract syntax tree)
+let variables = {};
+let definitions = [
+	'global','for','do','while','if','then','end','function',
+	'else', 'elseif', 'and', 'or','break','in','return','repeat','until',
+	'true','false','nil','js'
+] //keep a list of definitions for lunascript
+
+//lunascript tokens
+let tokens = [
+	'+','-','*','/','%','=','==','<=','>=','~=',
+	'>','<',
+	'[',']','{','}','^','..',
+
+]
+console.log(lexer);
+
+//parse input
+function parse(input) {
+	let keywords = [];
+	let comment = false;
+
+	//remove comments lol
+	for (let i in input) {
+		if (input[i].includes('--')) {
+			console.log(input[i]);
+			let index = input[i].indexOf('-');
+			input[i] = input[i].substring(0,index);
+		}
+	}
+		
+		
+
+
+
+	console.log(input);
+}
+
+parse(lexer);
